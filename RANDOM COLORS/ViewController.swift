0c53a29529ca06a263498b6c1efad3560de7ba14
Code Wizard <aiyaanmahajan@gmail.com>
2024-10-30 13:05:23 +0530

    Initial Commit

---
+++ RANDOM COLORS/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  RANDOM COLORS
+//
+//  Created by Aiyaan Mahajan on 30/10/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
